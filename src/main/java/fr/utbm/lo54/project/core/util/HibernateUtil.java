83933258f9e1139be94c42632316ed0ca1cefe83
pofilo <pofilo@pofilo.fr>
2016-04-15 18:26:17 +0200

    refactor main package, adding controllers and dao files, adding license and gitignore

--- src/main/java/fr/utbm/lo54/project/core/util/HibernateUtil.java
+++ src/main/java/fr/utbm/lo54/project/core/util/HibernateUtil.java
@@ -1,9 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.utbm.lo54.project.projectLo54.core.util;
+package fr.utbm.lo54.project.core.util;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.SessionFactory;
@@ -12,7 +23,6 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author Vincent
  */
 public class HibernateUtil {
 
